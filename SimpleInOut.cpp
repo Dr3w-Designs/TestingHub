f900c7ccff1cd50b997da183c039c38002e1cb2c
202410401@fit.edu.ph <202410401@fit.edu.ph>
2024-11-22 10:33:21 +0800

    newww

---
+++ SimpleInOut.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char name;
+
+
+    cout << "Hello I am Drew, what's your name?" << endl;
+    cin >> name;
+
+    cout << "Hi " << name << endl;
+
+    return 0;
+}
