3224cdb61bff98ac206e93e2172d9da11ab5f72e
202410401@fit.edu.ph <202410401@fit.edu.ph>
2024-11-22 10:43:24 +0800

    conditional

---
+++ NewFolder/new.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char answer;
+
+
+    cout << "Hello I am Drew, Do you wanna be friends? (1 yes, 2 no)" << endl;
+    cin >> answer;
+
+    if (1)
+    {
+        cout << "Hi " << answer << endl;
+    } else {
+        cout << "Go away!" << endl;
+    }
+    
+    
+
+    return 0;
+}
