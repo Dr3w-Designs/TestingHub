5e4d4dcd122c037c5d2ad8eef439268a05c8def5
202410401@fit.edu.ph <202410401@fit.edu.ph>
2024-11-26 11:15:30 +0800

    packages

--- TravelAgency.cpp
+++ TravelAgency.cpp
@@ -3,11 +3,32 @@
 
 using namespace std;
 
-//Travel Agencyß
+/*
+Create a system that provides of option of travel agency packages 
+-P1 = Coron: PHP 7k
+-P2 = Boracay: PHP 10k
+-P3 = Siargao: PHP 7.5k
+
+this Price is for 1 person only
+
+Provides a discount if meet certain conditions
+-4+ pax: 15% discount
+overall discount, tour guide: PHP 5k, 
+exclusive hotel: PHP 10k
+
+add: Accept Payment through cash and card
+
+Can accept multiple client until the user indicated to close the system or accomodated a 5 successful transaction.
+*/
+
 
 int main()
 {
-
+    cout << "Welcome to Fly High Butteryfly Airlines! \n"
+         << "We currently have 3 Travel Packages to choose from:\n"
+         << "Package 1 = Coron: PHP7,000 \n"
+         << "Package 1 = Boracay: PHP10,000 \n"
+         << "Package 1 = Siargao: PHP7,500 \n"
 
     return 0;
 }
